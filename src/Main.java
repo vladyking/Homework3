74948862e78a0e518b1e0a0a68b2938a5c38c833
vladyking <gvladykin@gmail.com>
2022-08-11 01:51:23 +0300

    Homework 3

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int age = 18;
+        int workYears = 1;
+        int counts = 1;
+        if (age >= 18 && workYears >= 1)
+        System.out.println ("Мы готовы выдать вам дебетовую карту в нашем банке");
+        else
+        System.out.println ("Мы НЕ готовы выдать вам дебетовую карту в нашем банке");
+    }
+}
